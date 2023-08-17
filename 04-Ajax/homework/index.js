f28ff26bb89dfd7cfa0ec77fed6b9c3bfd0233da
ValentinoJerez <valenjerez69@gmail.com>
2023-08-17 14:50:54 -0300

    git commit -m "Ejercicios 03"

--- 04-Ajax/homework/index.js
+++ 04-Ajax/homework/index.js
@@ -1,29 +1,65 @@
-//Utiliza el evento click en un boton para hacer que al hacer click en el mismo aparezca en el DOM una lista con todos los amigos que el servidor nos devolvera,
-//al hacer un GET a la ruta http://localhost:5000/amigos
-
-//Usando jquery, traigo el boton:
-const [boton] = $('#boton');
-//Creo una funcion
-const listaAmigos = (response) => {
-    //Traigo el elemento
-    const [lista] = $('#lista');
-    //Response es un array, lo recorro y a cada amigo le creo una "li"
-    response.forEach((amigo) => {
-        const newLi = document.createElement('li');
-        //Le agrego el texto asi se ve:
-        newLi.innerText = amigo.name;
-        //Le agrego a la lista un "hijo"
-        lista.appendChild(newLi);
+//Genero funcion handler(Funcion manejadora):
+let friendsHandler = function(){
+//Creo la lista
+let lista = $("#lista")
+//"Vacio" la lista:
+lista.empty();
+    //Hago la peticion de get
+    $.get("http://localhost:5000/amigos", (response) => {
+    //Tomo la lista de amigos, los recorro y por cada elemento hago:
+    response.map(amigo => {
+        //Creo el elemento:
+        let elementoHTML = document.createElement("li")
+        //Genero texto:
+        elementoHTML.innerHTML = `${amigo.name}`;
+        //Anido el elemento dentro de otro
+        lista.append(elementoHTML);
+    });
     });
 }
 
-//Creo una funcion
-const showFriends = () => {
-    //Aca dentro sucede el GET:
-    $.get("http://localhost:5000/amigos", (listaAmigos))
-}
+//Mostrar lista
+//Funcion principal:
+$('#boton').on("click", friendsHandler);
 
+//Genero un buscador:
+//Funcion principal
+$("#search").on("click", () => {
+    //Tomo el id
+    let id = $('#input').val();
+    //Si recibo id ejecuto esto:
+    if(id){
+    //Hago GET, pero el valor va a ser cambiante
+    $.get(`http://localhost:5000/amigos/${id}`, (response) =>
+    {
+        //Inserto string usando metodo jquery
+        $("#amigo").html(`Nombre: ${response.name}`)
+    });
+    } else {
+        $("#amigo").html("Inserta un ID")
+    }
+})
 
-//Al boton le agrego el addEvenListener: 
-//1° Parametro: Accion 2° Parametro: Reaccion(Callback)
-boton.addEventListener('click', (showFriends) => {})
+//Boton delete
+$("#delete").on("click", () => {
+    //Selecciono ID, usando metodos de DOM
+    let id = document.querySelector("#inputDelete").value
+    //Me pregunto si tengo id:
+    if(id){
+        //Delete necesita de ajax
+        $.ajax({
+            type: "DELETE",
+            url:`http://localhost:5000/amigos/${id}`,
+            //Una vez que tengo la respuesta
+            success: () => {
+                $('#success').html("Tu amigo fue borrado");
+                //Invoco a la funcion manejadora
+                friendsHandler()
+                //El input se debe borrar, despues de click
+                id = ""; //HACER FUNCION QUE LO BORRE
+            },
+        })
+    } else {
+        $('#success').html("Inserta un ID");
+    }
+})
