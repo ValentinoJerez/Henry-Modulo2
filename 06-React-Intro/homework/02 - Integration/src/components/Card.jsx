9756a8e84de986b2b5a3ccdb9bbf994ae02274ed
ValentinoJerez <valenjerez69@gmail.com>
2023-08-21 12:02:35 -0300

    Ejercicios 05

--- 06-React-Intro/homework/02 - Integration/src/components/Card.jsx
+++ 06-React-Intro/homework/02 - Integration/src/components/Card.jsx
@@ -1,13 +1,14 @@
 export default function Card(props) {
+   //Extraemos informacion de cada personaje desde props:
+   const {character, onClose} = props;
    return (
       <div>
-         {/* <button onClick={}>X</button>
-         <h2></h2>
-         <h2></h2>
-         <h2></h2>
-         <h2></h2>
-         <h2></h2>
-         <img src={} alt='' /> */}
+          <button onClick={onClose}>X</button>
+         <h2>Name: {character.name}</h2>
+         <h2>Status: {character.status}</h2>
+         <h2>Species: {character.species}</h2>
+         <h2>Gender: {character.gender}</h2>
+         <img src={character.image} alt={character.name} />
       </div>
    );
 }
--- 06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
+++ 06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
@@ -1,5 +1,13 @@
 import Card from './Card';
 
 export default function Cards(props) {
-   return <div></div>;
+  const {characters}=props;
+  const {onClose}=props;
+   return (
+   <div>
+      {characters.map((character) =>( 
+      <Card character= {character} onClose={onClose}/>
+      ))}
+   </div>
+   );
 }
